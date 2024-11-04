18b7ff00a07485351d6bc8af113e31c81417af32
Andre-Arcilla <aaarcilla123@gmail.com>
2024-11-04 18:37:30 +0800

    Initial commit

---
+++ tetris/BlockQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        public Block NextBlock {  get; private set; }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
